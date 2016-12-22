49933196624f28ee73009a962feefc45b13ca7b7
kfkitsune <kfkitsune@users.noreply.github.com>
2016-12-22 15:36:26 -0500

    Modularize (#2)
    Splits out duplicated code into an additional module.
    
    * Create KFK-CommonFunctions.psm1
    
    * Update IAVM Main.ps1
    
    * Update Nessus Plugin and IAVM Mappings.ps1
    
    * Update Generate IAVM Summary Information.ps1
    
    * Update Download New IAVM HTML Files.ps1

--- sc-iavm/Nessus Plugin and IAVM Mappings.ps1
+++ sc-iavm/Nessus Plugin and IAVM Mappings.ps1
@@ -6,6 +6,15 @@ param(
     Plugin->IAVM mapping, and IAVM->Plugin mapping
 #>
 
+try {  ### Begin module import block ###
+    Import-Module $env:USERPROFILE\Documents\AuthScripts\Modules\KFK-CommonFunctions.psm1 -ErrorAction Stop -DisableNameChecking
+}
+catch [System.IO.FileNotFoundException] {
+    Write-Host -ForegroundColor Red "Unable to load required module... terminating execution..."
+    Start-Sleep -Seconds 5
+    exit
+}      ### End module import block ###
+
 $scURI = ""; #Optional; Use if not using external cred/conf file.
 $chosenCertThumb = "";
 if ($paramPKIThumbprint) { [string]$chosenCertThumb = $paramPKIThumbprint }
@@ -58,7 +67,9 @@ function Read-ConfigFile {
 function Output-Debug { #Simple output if we are debugging.
     param($req)
     if ($scriptDebug) {
-        Write-Host $req
+        $Global:DebugPreference = "Continue"
+        Write-Debug $req
+        $Global:DebugPreference = "SilentlyContinue"
     }
 }
 function SC-GetCredentials {
@@ -188,31 +199,8 @@ function SC-Get-IAVT {
     SC-Connect "plugin" "init" $json;
     $Script:scIAVTs = $Script:scResponse.response.plugins;
 }
-function PS-CertificateChooser {
-    <# Obtain listing of (potentially) valid certificates user has for authentication purposes #>
-    Push-Location
-    Set-Location cert:
-    Set-Location \
-    Set-Location Cert:\CurrentUser\My
-    $certificateListing = Get-ChildItem
-    Pop-Location
 
-    <# Prompt the user for which certificate to use #>
-    Write-Host("Type the number of the certificate you wish to use for authentication.")
 
-    <# User Input: Choose which cert to use
-        CHOSEN NUMBER MUST BE DECREMENTED BY ONE (1). POWERSHELL COUNTS CORRECTLY (0, 1, 2, ...)
-        Starts at '1' for human-readability...
-    #>
-    $i = 1;
-    foreach($z in $certificateListing) {
-        Write-Host("[" + $i.ToString() + "] ::: " + $z.Subject + " ::: " + $z.Thumbprint)
-        $i++;
-    }
-    $in = (Read-Host "Enter the number of the certificate to use, as shown above in brackets; e.g., '1'").ToInt32($null) - 1;
-
-    return $certificateListing.Get($in).Thumbprint #<--End state for this function
-}
 function Process-Xref-String {
     param([string]$Type, 
           [string]$Xrefs,
@@ -249,60 +237,13 @@ function Process-Xref-String {
         }
     }
 }
-function Pause-Script ($Message = "Press any key to continue . . . ") {
-	If ($psISE) {
-		# The "ReadKey" functionality is not supported in Windows PowerShell ISE.
-
-		$Shell = New-Object -ComObject "WScript.Shell"
-		$Button = $Shell.Popup("Click OK to continue.", 0, "Script Paused", 0)
-
-		Return
-	}
-
-	#Write-Host -NoNewline $Message
-
-	$Ignore =
-		16,  # Shift (left or right)
-		17,  # Ctrl (left or right)
-		18,  # Alt (left or right)
-		20,  # Caps lock
-		91,  # Windows key (left)
-		92,  # Windows key (right)
-		93,  # Menu key
-		144, # Num lock
-		145, # Scroll lock
-		166, # Back
-		167, # Forward
-		168, # Refresh
-		169, # Stop
-		170, # Search
-		171, # Favorites
-		172, # Start/Home
-		173, # Mute
-		174, # Volume Down
-		175, # Volume Up
-		176, # Next Track
-		177, # Previous Track
-		178, # Stop Media
-		179, # Play
-		180, # Mail
-		181, # Select Media
-		182, # Application 1
-		183  # Application 2
-
-	While ($KeyInfo.VirtualKeyCode -Eq $Null -Or $Ignore -Contains $KeyInfo.VirtualKeyCode) {
-		$KeyInfo = $Host.UI.RawUI.ReadKey("NoEcho, IncludeKeyUp")
-	}
-
-	Write-Host
-}
 
 Read-ConfigFile;
 SC-GetCredentials;
 
 if ($chosenCertThumb -eq "") {  # Only execute if we don't have a thumbprint from the commandline
     <# WHO AM I? //WHAT YEAR IS IT?!// #>
-    $chosenCertThumb = PS-CertificateChooser
+    $chosenCertThumb = Invoke-CertificateChooser
     Write-Host("Chosen certificate thumbprint ::: " + $chosenCertThumb)
 }
 
