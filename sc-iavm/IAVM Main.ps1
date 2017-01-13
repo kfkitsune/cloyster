704c88700670e5c859261ca1753cdb096f20fc37
kfkitsune <kfkitsune@users.noreply.github.com>
2017-01-13 08:55:07 -0500

    Allow script to use default settings
    Further automate the automation automating the automation.

--- sc-iavm/IAVM Main.ps1
+++ sc-iavm/IAVM Main.ps1
@@ -18,6 +18,12 @@ Write-Host ""
 
 $selectedCertificateThumbprint = Invoke-CertificateChooser
 Write-Host "Using thumbprint:" $selectedCertificateThumbprint -ForegroundColor Gray
+Write-Host ""
+Write-Host "Do you want to use default settings, when possible? Currently, this means retrieving the last 7 days of IAVM HTML files."
+$use_defaults = Read-Host -Prompt "Enter (Y) or (N); other values default to no"
+if ($use_defaults.ToLower() -eq "y") { $use_defaults = $true }
+else { $use_defaults = $false }
+
 
 $i = 0  # Current script number.
 $j = 3  # Total scripts to launch.
@@ -28,7 +34,7 @@ Write-Host "("(++$i)"/"($j)") IAVM/Plugin Mapping" -ForegroundColor Cyan
 
 Write-Host ""
 Write-Host "("(++$i)"/"($j)") IAVM HTML Download" -ForegroundColor Cyan
-& '.\Download New IAVM HTML Files.ps1' -paramPKIThumbprint $selectedCertificateThumbprint
+& '.\Download New IAVM HTML Files.ps1' -paramPKIThumbprint $selectedCertificateThumbprint -paramUseDefaults $use_defaults
 
 Write-Host ""
 Write-Host "("(++$i)"/"($j)") IAVM Summary Information" -ForegroundColor Cyan
