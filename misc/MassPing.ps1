93ec395cbdbc50aa0ace51584348be396087207c
Kyra <kfkitsune@users.noreply.github.com>
2017-02-09 13:31:06 -0500

    Add AD OU LDAP String option for getting DNSNames to ping

--- misc/MassPing.ps1
+++ misc/MassPing.ps1
@@ -2,13 +2,52 @@
 A mass-ping script.
 
 Using a source hostname/IP file, use Test-Connection to check if the targeted systems are online or not.
+
+If hostname file is used, it should be made with one IP/hostname/DNS name per line.
 #>
 
-$hostname_list = Get-Content .\hostnames.txt
 $job_running = $true
 $subset_increment = 250
 $index_lower = 0
 $index_upper = $subset_increment
+
+function Verify-ADOUExists {
+    param([string]$ou)
+    try {
+        [adsi]::Exists("LDAP://$ou")
+    }
+    catch [System.Management.Automation.RuntimeException] {
+        return $false
+    }
+}
+
+# How are we doing this ping today?
+$input = Read-Host -Prompt "To determine which systems are online...
+    Do you want to: [1] Use .\hostnames.txt in the current directory
+                    [2] Provide an AD LDAP OU string?
+    Enter choice"
+if ($input -eq 1){
+    $hostname_list = Get-Content .\hostnames.txt
+}
+elseif ($input -eq 2) {
+    Import-Module ActiveDirectory
+    $ou = Read-Host -Prompt "Enter the AD OU LDAP string to act as the searchbase"
+    # Keep trying to get a valid OU
+    while (!(Verify-ADOUExists -ou $ou)) {
+        Write-Host -ForegroundColor Red "The OU had an issue of some kind; check syntax, check if AD is up, etc. Try again. Ctrl+C to cancel."
+        $ou = Read-Host -Prompt "Enter the AD OU LDAP string to act as the searchbase"
+    }
+    Write-Host -ForegroundColor Cyan "Getting AD Computer names..."
+    $ad_results = Get-ADComputer -SearchBase $ou -SearchScope Subtree -Filter "*"
+    # Get the information in the format we would expect as if we got the list straight from a file.
+    $hostname_list = @()
+    foreach ($dns in ($ad_results | Where-Object { $_.DNSHostName -ne $null } | Select-Object DNSHostName)) {
+        $hostname_list += $dns.DNSHostName
+    }
+}
+else {
+    exit
+}
 
 # Thread-out all the ping chunks; WMI does not like super huge ping jobs, so break it up for the old geezer that WMI is (and PS doesn't handle it)
 while ($index_lower -le $hostname_list.Length) {
@@ -32,7 +71,7 @@ foreach ($job in $completed_jobs) {
     $results += $job | Receive-Job | Where-Object { $_.StatusCode -eq 0 } | Select-Object Address, IPv4Address
 }
 
- # Get octets (($storage | Sort-Object -Property Octet1,Octet2,Octet3,Octet4 | ft -AutoSize))
+# Get octets (($storage | Sort-Object -Property Octet1,Octet2,Octet3,Octet4 | ft -AutoSize))
 [regex]$regex_ip_pattern = [regex]"(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})"
 $storage = @()
 foreach ($result in $results) {
