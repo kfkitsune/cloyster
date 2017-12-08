71e746b7d966a243f414413544e5d79f325f76eb
Kyra <kfkitsune@users.noreply.github.com>
2017-12-08 12:18:15 -0500

    Update sc.api.core.psm1

--- sc-iavm/api/sc.api.core.psm1
+++ sc-iavm/api/sc.api.core.psm1
@@ -5,28 +5,41 @@
     a) https://docs.tenable.com/sccv/api/index.html
     b) https://support.tenable.com/support-center/cerberus-support-center/includes/widgets/sc_api/index.html
 #>
+#$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
 try {  ### Begin module import block ###
-    Import-Module -Name @(
-        '.\modules\sc_api_modules\sc.communication.psm1',
-        '.\modules\sc_api_modules\sc.asset.psm1',
-        '.\modules\sc_api_modules\sc.auditfile.psm1',
-        '.\modules\sc_api_modules\sc.credential.psm1',
-        '.\modules\sc_api_modules\sc.feed.psm1',
-        '.\modules\sc_api_modules\sc.file.psm1',
-        '.\modules\sc_api_modules\sc.plugin.psm1',
-        '.\modules\sc_api_modules\sc.policy.psm1',
-        '.\modules\sc_api_modules\sc.reports.psm1',
-        '.\modules\sc_api_modules\sc.repository.psm1',
-        '.\modules\sc_api_modules\sc.rolesgroupsusers.psm1',
-        '.\modules\sc_api_modules\sc.scan.psm1',
-        '.\modules\sc_api_modules\sc.scanResult.psm1',
-        '.\modules\sc_api_modules\sc.zone.psm1',
-        '.\modules\sc_api_modules\utils.psm1'
-    ) -DisableNameChecking
+    # Test for constrained language mode
+    [void][Math]::Abs(1)
+
+    # Modules to load`
+    $modules = @(
+        "$PSScriptRoot\sc_api_modules\sc.asset.psm1",
+        "$PSScriptRoot\sc_api_modules\sc.auditfile.psm1",
+        "$PSScriptRoot\sc_api_modules\sc.communication.psm1",
+        "$PSScriptRoot\sc_api_modules\sc.credential.psm1",
+        "$PSScriptRoot\sc_api_modules\sc.feed.psm1",
+        "$PSScriptRoot\sc_api_modules\sc.file.psm1",
+        "$PSScriptRoot\sc_api_modules\sc.plugin.psm1",
+        "$PSScriptRoot\sc_api_modules\sc.policy.psm1",
+        "$PSScriptRoot\sc_api_modules\sc.reports.psm1",
+        "$PSScriptRoot\sc_api_modules\sc.repository.psm1",
+        "$PSScriptRoot\sc_api_modules\sc.rolesgroupsusers.psm1",
+        "$PSScriptRoot\sc_api_modules\sc.scan.psm1",
+        "$PSScriptRoot\sc_api_modules\sc.scanResult.psm1",
+        "$PSScriptRoot\sc_api_modules\sc.zone.psm1",
+        "$PSScriptRoot\sc_api_modules\utils.psm1"
+    )
+    foreach ($module in $modules) {
+        Import-Module $module -DisableNameChecking -ErrorAction Stop
+    }
 }
 catch [System.IO.FileNotFoundException] {
     Write-Host -ForegroundColor Red "Unable to load required module... terminating execution..."
     Start-Sleep -Seconds 5
     exit
-} ### End module import block ###
-
+}
+catch {
+    if ($_.FullyQualifiedErrorID -eq "MethodInvocationNotSupportedInConstrainedLanguage") {
+        throw "The SecurityCenter API modules will not function properly in constrained language mode."
+    }
+}
