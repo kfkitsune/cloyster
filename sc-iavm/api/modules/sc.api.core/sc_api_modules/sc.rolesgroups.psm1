e40f8325e60a3ffce62be9c83182a0376d19dca8
Kyra <kfkitsune@users.noreply.github.com>
2018-05-07 08:32:09 -0400

    Update and rename sc.rolesgroupsusers.psm1 to sc.rolesgroups.psm1
    The Cerberus variant of the API documentation was finally killed off too (now that page is behind HTTP authentication).

--- sc-iavm/api/modules/sc.api.core/sc_api_modules/sc.rolesgroups.psm1
+++ sc-iavm/api/modules/sc.api.core/sc_api_modules/sc.rolesgroups.psm1
@@ -2,59 +2,9 @@
     Contains functions pertaining to Users, Groups, and Roles.
 
     Contains the following endpoints:
-     - user
      - group
      - role
 #>
-
-
-function SC-Get-User() {
-    <#
-        Retrieves user information from the SecurityCenter, returning the specified fields.
-    #>
-    param(
-        [switch]$username,
-        [switch]$firstname,
-        [switch]$lastname,
-        [switch]$status,
-        [switch]$role,
-        [switch]$title,
-        [switch]$email,
-        [switch]$address,
-        [switch]$city,
-        [switch]$state,
-        [switch]$country,
-        [switch]$phone,
-        [switch]$fax,
-        [switch]$createdTime,
-        [switch]$modifiedTime,
-        [switch]$lastLogin,
-        [switch]$lastLoginIP,
-        [switch]$mustChangePassword,
-        [switch]$locked,
-        [switch]$failedLogins,
-        [switch]$authType,
-        [switch]$fingerprint,
-        [switch]$password,
-        [switch]$description,
-        [switch]$canUse,
-        [switch]$canManage,
-        [switch]$managedUsersGroups,
-        [switch]$managedObjectsGroups,
-        [switch]$preferences,
-        [switch]$ldaps,
-        [switch]$ldapUsername
-    )
-    $dict = @{ "fields" = "id" }
-    # Dynamically read the passed switches for the returned instead of a seperate line for each
-    foreach ($key in $PSBoundParameters.Keys) {
-        if ($key -notin @('id')) {
-            $dict.Set_Item("fields", $dict.Get_Item("fields") + ",$key")
-        }
-    }
-
-    return SC-Connect -scResource user -scHTTPMethod GET -scQueryStringDict $dict
-}
 
 
 function SC-Get-GroupInformation() {
@@ -106,7 +56,6 @@ function SC-Get-RoleInformation() {
         Returns: As in the function description.
 
         https://docs.tenable.com/sccv/api/Role.html
-        https://support.tenable.com/support-center/cerberus-support-center/includes/widgets/sc_api/Role.html
     #>
     param(
         [switch]$name,
