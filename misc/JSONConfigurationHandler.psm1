046d793c731ed5b12a1f6ec46f7b38c7277e191d
Kyra <kfkitsune@users.noreply.github.com>
2019-03-06 15:07:31 -0500

    Update JSONConfigurationHandler.psm1

--- misc/JSONConfigurationHandler.psm1
+++ misc/JSONConfigurationHandler.psm1
@@ -1,12 +1,20 @@
 function Read-JSONConfigurationFile() {
     <#
         Reads a JSON configuration file, and returns a PoSH dictionary of the deseralized JSON content.
+
+        Optionally, retrieves a single top-level key from the deserialized JSON. Useful for retrieving
+        a configuration 'section'.
+
+        Parameters:
+          - FilePath: String. The full path and filename to a JSON configuration file.
+          - Section: String. Retrieves a specified top-level value from the deserialized JSON.
     #>
     param(
         [Parameter(Mandatory=$true)]
-          [String]$FileName
+          [String]$FilePath,
+        [String]$Section
     )
-    $file_content = Get-Content -Raw -Path $FileName
+    $file_content = Get-Content -Raw -Path $FilePath
 
     if ($PSVersionTable.PSVersion.Major -ge 6) {
         # -AsHashtable introduced in PoSH v6.0
@@ -17,7 +25,12 @@ function Read-JSONConfigurationFile() {
         (ConvertFrom-Json $file_content).PSObject.Properties | ForEach { $hashtable[$_.Name] = $_.Value }
     }
     
-    return $hashtable
+    if ($Section) {
+        return $hashtable.$Section
+    }
+    else {
+        return $hashtable
+    }
 }
 
 function Write-JSONConfigurationFile() {
@@ -28,20 +41,18 @@ function Write-JSONConfigurationFile() {
         Parameters:
         - ConfObj: HashTable. The PoSH dictionary/HT to deserialize as JSON and write out.
         - FilePath: String. The path of the file to write out.
-        - Clobber: Switch. If set, overwrites the targeted file at $FilePath, if it exists.
+        - NoClobber: Switch. If set, does not clobber any existing file at the specified
+          FilePath (as per `Out-File -NoClobber).
+        - Compress: Switch. If set, compresses the serialized JSON output (as
+          `ConvertTo-Json -Compress` would).
     #>
     param(
         [Parameter(Mandatory=$true)]
           [HashTable]$ConfObj,
         [Parameter(Mandatory=$true)]
           [String]$FilePath,
-        [switch]$Clobber
+        [switch]$NoClobber,
+        [switch]$Compress
     )
-    if (!$Clobber.IsPresent) {
-        # Don't clobber the target filename (if it exists)
-        ConvertTo-Json -Depth 100 $ConfObj | Out-File -FilePath $FilePath -NoClobber
-    }
-    else {
-        ConvertTo-Json -Depth 100 $ConfObj | Out-File -FilePath $FilePath
-    }
+    ConvertTo-Json -Depth 100 -Compress:$Compress | Out-File -FilePath $FilePath -NoClobber:$NoClobber
 }
