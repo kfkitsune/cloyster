2d4af5e7e0dd93d7b5ddaee82e7a7c790600905e
Kyra <GitHub.com@KyraVixen.com>
2020-04-01 14:50:38 -0400

    Add version information to the retrieved OS versions
    It's slow, but... it works. There's probably a faster way to do this than iterating over the entire $ad_results, but for the moment this works.

--- misc/GetTypesAndCountOfSystemsFromAD.ps1
+++ misc/GetTypesAndCountOfSystemsFromAD.ps1
@@ -1,7 +1,7 @@
 <#
 Retrieve a count of operating systems as determined by the OperatingSystem field of an AD Computer object.
 
-Limitations: The AD Computer object(s)' OperatingSystem property must be set to obtain a correct number.
+Limitations: The AD Computer object(s)' OperatingSystem property must be set correctly to obtain a correct number.
 #>
 Import-Module ActiveDirectory
 
@@ -13,23 +13,30 @@ if (!$ou) {  # If `$ou` is not set
     for ($i = 1; $i -lt $Local:sys_dn_split.Length; $i++) { Write-Host '['$i' ]' $Local:sys_dn_split.Get([int]$i) }
     $Local:input = [int](Read-Host -Prompt "Enter the OU level to use as the SearchBase")
     # Rejoin the OU segments into a new OU string
-    Clear-Variable -Scope Local -Name source_ou  # Clear this beforehand to guard against multiple runs in the same session breaking things.
+    Clear-Variable -Scope Local -Name source_ou -ErrorAction SilentlyContinue # Clear this beforehand to guard against multiple runs in the same session breaking things.
     foreach ($pos in $Local:input..($Local:sys_dn_split.Length - 1)) { $Local:source_ou += $Local:sys_dn_split.Get($pos) + ',' }
     $ou = $Local:source_ou.TrimEnd(',')
 }
 
 Write-Host -ForegroundColor Cyan "Getting information from AD; this may take a moment..."
-$ad_results = Get-ADComputer -SearchBase $ou -SearchScope Subtree -Filter "*" -Properties "operatingSystem"
+$ad_results = Get-ADComputer -SearchBase $ou -SearchScope Subtree -Filter "*" -Properties OperatingSystem,OperatingSystemVersion
 Write-Host -ForegroundColor Cyan "AD Pull complete; continuing...`n`r"
 
 $operating_systems_count = @{}
 
 # Select each OS name set in the system properties, where it is not null...
-foreach ($os_name in ( $ad_results | Select operatingSystem -Unique | Where-Object { $_.OperatingSystem -ne $null } ) ) {
-    # Then determine the count of those OSes...
-    $Local:value = ($ad_results | Where-Object { $_.OperatingSystem -eq $os_name.operatingSystem } | Measure-Object).Count
-    # Then store that count for later output.
-    $operating_systems_count += @{$os_name.operatingSystem = $Local:value}
+# Extract the unique OSes
+$os_names = ($ad_results | Select-Object -Unique operatingSystem | Where-Object {$_.operatingSystem -ne $null})
+foreach($os_name in $os_names) {
+    # Extract the versions for the OS
+    $os_versions = $ad_results | Where-Object {$_.operatingSystem -eq $os_name.operatingSystem} | Select -Unique operatingSystemVersion
+    foreach($os_version in $os_versions) {
+        $os_name_ver = $os_name.operatingSystem + " | Version: " + $os_version.operatingSystemVersion
+        # Get the count of OS per OS version
+        $count = (($ad_results | Where-Object {($_.operatingSystem -eq $os_name.operatingSystem) -and ($_.operatingSystemVersion -eq $os_version.operatingSystemVersion)}) | Measure-Object).Count
+        $operating_systems_count += @{$os_name_ver = $count}
+        Write-Host -foregroundcolor Gray "Processed $os_name_ver ..."
+    }
 }
 
 Write-Host "---------- Raw Results ----------" -ForegroundColor Green
@@ -38,7 +45,9 @@ $operating_systems_count.GetEnumerator() | Sort-Object -Property Name | Format-T
 # Store the Server/Workstation OS'es to group together
 $server_scriptblock = [System.Management.Automation.ScriptBlock]{
     $_.OperatingSystem -like "Windows Server 2008 R2 *" -or
-    $_.OperatingSystem -like "Windows Server 2012 R2 *" 
+    $_.OperatingSystem -like "Windows Server 2012 R2 *" -or
+    $_.OperatingSystem -like "Windows Server 2016 *" -or
+    $_.OperatingSystem -like "Windows Server 2019 *"
 }
 $workstation_scriptblock = [System.Management.Automation.ScriptBlock]{
     $_.OperatingSystem -eq "Windows 7 Enterprise" -or
