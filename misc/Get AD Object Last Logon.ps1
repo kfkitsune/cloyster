a2d0b5ea7c415f31d5ebd25f0efcd2d1072f95a6
Kyra <GitHub.com@KyraVixen.com>
2020-06-26 09:01:43 -0400

    Add pwdLastSet and user/computer selector prompt
    - Adds the pwdLastSet as a property, since it can act as an additional data-point to determine if an object is stale.
    - Adds a simple prompt for alternating between getting user objects or computer objects.
    - Changes the domain controller prefix variable to use a regular expression for allowing additional sites to be included in the results (since not every property on objects replicates).
    - Leverages the ``$stopwatch`` object created, and the method used elsewhere in the script to only update the ``Write-Progress`` results every ~500ms, since writing progress itself does take a non-negligible quantity of time.

--- misc/Get AD Object Last Logon.ps1
+++ misc/Get AD Object Last Logon.ps1
@@ -1,9 +1,7 @@
 # The prefix for the domain controllers to check (e.g., "NYC" for NYC001, NYC002, NYC003)
-$target_dc_prefix = "NYC"
+$target_dc_prefix_regex = "^(NYC|SJC|JPN).*"
 # The AD SearchBase (i.e., OU) to constrain getting objects from (e.g., OU=Users,DC=contoso,DC=com)
 $target_ad_searchbase = "OU=Main,DC=contoso,DC=com"
-# Only return users older than the specified days (via Get-ADUser filter)
-$days_for_filtering = 30
 
 
 # Module imports
@@ -24,11 +22,23 @@ catch [System.IO.FileNotFoundException] {
 
 # Get a listing of all domain controllers for the current domain
 $ad_information = Get-ADDomain
-$target_domain_controllers = $ad_information.ReplicaDirectoryServers -like ($target_dc_prefix + "*")
+$target_domain_controllers = $ad_information.ReplicaDirectoryServers -cmatch $target_dc_prefix_regex
 $date_now = Get-Date
 
-# Get a list of candidate users older than the filtered time
-# $filter_datetime = Convert-DateTimeToFileTime -datetime (Get-Date).AddDays(-1 * $days_for_filtering)
+Write-Host -ForegroundColor Cyan "Do you want to retrieve last logon dates for:"
+Write-Host -ForegroundColor Cyan "[1] User objects"
+Write-Host -ForegroundColor Cyan "[2] Computer objects"
+$input = [int](Read-Host -Prompt "Enter [1] or [2]; other values assume computer; <Ctrl+C> to exit")
+if ($input -eq 1) {
+    $object_type = "user"
+}
+elseif ($input -eq 2) {
+    $object_type = "computer"
+}
+else {
+    Write-Host -ForegroundColor Yellow "Invalid input; assuming user objects"
+    $object_type = "user"
+}
 
 # Initialize the AD query storage location...
 $datastore = [System.Collections.Generic.List[System.Object]]::new(1000000)
@@ -37,13 +47,14 @@ $datastore = [System.Collections.Generic.List[System.Object]]::new(1000000)
 foreach($dc in $target_domain_controllers) {
     # Build out the AD searcher [Ref: https://stackoverflow.com/a/60419117]
     # Why manual? Because this takes F O R E V E R when using the cmdlets
-    $ad_searcher = [adsisearcher]::new([adsi]"LDAP://$dc/$target_ad_searchbase", "(objectCategory=user)")
+    $ad_searcher = [adsisearcher]::new([adsi]"LDAP://$dc/$target_ad_searchbase", "(objectCategory=$object_type)")
     $ad_searcher.PropertiesToLoad.Add("name") > $null
     $ad_searcher.PropertiesToLoad.Add("samaccountname") > $null
     $ad_searcher.PropertiesToLoad.Add("distinguishedName") > $null
     $ad_searcher.PropertiesToLoad.Add("userAccountControl") > $null  # Ref: https://stackoverflow.com/a/47099079
     $ad_searcher.PropertiesToLoad.Add("lastlogon") > $null
     $ad_searcher.PropertiesToLoad.Add("lastlogontimestamp") > $null
+    $ad_searcher.PropertiesToLoad.Add("pwdLastSet") > $null
     $ad_searcher.PropertiesToLoad.Add("objectClass") > $null
     $ad_searcher.PageSize = 1000
 
@@ -53,12 +64,17 @@ foreach($dc in $target_domain_controllers) {
     $results | Measure-Object > $null  # Because it wants to die mid-processing and/or not retrieve data until actual use?
     Write-Host -ForegroundColor DarkGray ("[DEBUG] Data retrieval took "+ $stopwatch.Elapsed.TotalSeconds + " seconds; " + $results.Count + " objects found")
 
-    $counter = 0
+    $count = 0; $activity = "Storing data [$dc]"
     foreach($obj in $results) {
-        Write-Progress -id 0 -Activity "Storing data [$dc]" -CurrentOperation $obj.Name -PercentComplete (100* ($counter/$results.Count))
+        if ($stopwatch.Elapsed.TotalMilliseconds -ge 500) {
+            Write-Progress -id 0 -Activity $activity -CurrentOperation "$count records processed" -PercentComplete (100 * ($count / $results.Count))
+            $stopwatch.Restart()
+        }
+
         if ($obj.Properties.objectclass -contains "contact") {
             continue  # Skip contact AD Objects...
         }
+
         # Load the resulting information into the data store
         $data = [PSCustomObject]@{
             'name' = $obj.Properties['name'][0].ToString()
@@ -66,17 +82,19 @@ foreach($dc in $target_domain_controllers) {
             'distinguishedName' = $obj.Properties['distinguishedname'][0].ToString()
             'enabled' = ($obj.Properties['useraccountcontrol'].Item(0) -band 2) -ne 2
             'lastLogon' = $obj.Properties['lastlogon'][0]
+            'pwdLastSet' = $obj.Properties['pwdLastSet'][0]
             'lastLogonTimestamp' = $obj.Properties['lastlogontimestamp'][0]
             '_domain_controller' = $dc
         }
         $datastore.Add($data)
-        $counter += 1
+        $count += 1
     }
     $ad_searcher.Dispose()
-    Write-Progress -id 0 -complete -Activity "Storing data [$dc]"
+    Write-Progress -id 0 -complete -Activity $activity
 }
 
 # Combine the results from each DC down to a single collection
+Write-Host -ForegroundColor Gray "[INFO] Grouping records, this might take a moment..."
 $grouped_results = $datastore | Group-Object -Property samaccountname
 
 
@@ -85,7 +103,7 @@ $count = 0; $progress_activity = "Processing last logon dates"; $stopwatch.Reset
 foreach($result in $grouped_results) {
     if ($stopwatch.Elapsed.TotalMilliseconds -ge 500) {
         Write-Progress -id 0 -Activity $progress_activity -CurrentOperation "$count records processed" -PercentComplete (100 * ($count / $grouped_results.Count))
-        $stopwatch.Reset(); $stopwatch.Start()
+        $stopwatch.Restart()
     }
 
     # Ignore service accounts or other things to avoid
@@ -101,6 +119,10 @@ foreach($result in $grouped_results) {
     if ($lastlogontimestamp_datetimes) {
         $latest_lastlogontimestamp_date = ($lastlogontimestamp_datetimes | Sort-Object -Descending)[0]
     }
+    $pwdlastset_datetimes = $result.Group | ForEach-Object {Convert-FileTimeToDateTime($_.pwdLastSet)}
+    if ($pwdlastset_datetimes) {
+        $latest_pwdlastset_date = ($pwdlastset_datetimes | Sort-Object -Descending)[0]
+    }
 
     $data = [PSCustomObject]@{
         "Name" = $result.Group[0].name
@@ -110,8 +132,11 @@ foreach($result in $grouped_results) {
         "Days Since Last Login" = (New-TimeSpan -Start $latest_logon_date -End $date_now).Days
         "Latest Last Logon Timestamp (Replicates)" = $latest_lastlogontimestamp_date
         "Days Since Latest Last Logon Timestamp" = (New-TimeSpan -Start $latest_lastlogontimestamp_date -End $date_now).Days
+        "Account Password Last Set" = $latest_pwdlastset_date
+        "Days Since Account Password Last Set" = (New-TimeSpan -Start $latest_pwdlastset_date -End $date_now).Days
         "All Available LastLogon Dates" = $lastlogon_datetimes -join " | "
         "All Available LastLogonTimestamp Dates" = $lastlogontimestamp_datetimes -join " | "
+        "All Available PwdLastSet Dates" = $pwdlastset_datetimes -join " | "
         "distinguishedName" = $result.Group[0].distinguishedName
     }
     $datastore.Add($data)
